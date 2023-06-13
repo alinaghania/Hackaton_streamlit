e94574bc04a66ad8f2a25a695f1a8d5e479e0ee6
Alina Ghani <alina@MacBook-Air-de-Alina.local>
2023-06-13 13:48:21 +0200

    second push

--- app.py
+++ app.py
@@ -21,5 +21,30 @@ pages = st.sidebar.selectbox(
 
 # Display image in Interpretation of the data page
 if pages == "Interpretation of the data":
-    image_path = "/Users/alina/Desktop/Hackaton/1.png"
+    image_path = "/Users/alina/Desktop/Hackaton/Affiche Hackathon LMG Efrei V2.png"
     display_image(image_path)
+
+    st.write("")
+
+    image_path1 = "/Users/alina/Desktop/Hackaton/1.png"
+    display_image(image_path1)
+
+    st.write("")
+
+    image_path2 = "/Users/alina/Desktop/Hackaton/2.png"
+    display_image(image_path2)
+
+    st.write("")
+
+    image_path3 = "/Users/alina/Desktop/Hackaton/3.png"
+    display_image(image_path3)
+
+    st.write("")
+
+    image_path4 = "/Users/alina/Desktop/Hackaton/4.png"
+    display_image(image_path4)
+
+    st.write("")
+
+    image_path5 = "/Users/alina/Desktop/Hackaton/6.png"
+    display_image(image_path5)
